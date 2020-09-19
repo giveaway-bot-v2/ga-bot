4363d24b7eb88c64c8247c9227373952fbd044f7
Jenk Scalia <41992596+PixelSized@users.noreply.github.com>
2020-09-19 15:30:48 +1000

    Update Index.js

--- Index.js
+++ Index.js
@@ -9,6 +9,21 @@ client.on('ready', () => {
     console.log("Serving in: " + client.guilds.cache.size)
 })
 
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.startsWith(client.user.toString())) {
+        // Send acknowledgement message
+        receivedMessage.channel.send("Hey there, " +
+            receivedMessage.author.toString() + "! My prefix is `!` ")
+    }
+})
+
 bot_secret_token = "token here"  //we may want to move to a .env later
 
 client.login(bot_secret_token)
