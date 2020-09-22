e664c36b8979037b6ee445cf3b4ce6834bb38496
Jenk Scalia <41992596+PixelSized@users.noreply.github.com>
2020-09-22 20:02:39 +1000

    Updated Sharding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { 
-  token: `process.env.TOKEN`,
+  token: "${process.env.TOKEN}',
   totalShards: 'auto',
   respawn: 'true',
   
