15493d821ff982bd68212a05026dffdce12ddc0f
Danny Harpigny <dannyhpy@disroot.org>
2021-07-20 14:17:18 +0200

    rep: Wrap give() method into a transaction

--- src/commands/rep.ts
+++ src/commands/rep.ts
@@ -22,20 +22,27 @@ export default class RepCommand extends Command {
 
   async give(interaction: CommandInteraction, increment: number): Promise<void> {
     const conn = await interaction.client.db.connect();
-    
-    const lastGiveaway = await interaction.client.db.giveaways.getLastWon(interaction.user.id);
-    if (lastGiveaway == null) return interaction.reply("You cannot use this command at the moment.");
-    if (lastGiveaway.rep_given === true) {
-      return interaction.reply('A reputation point has already been given to this donor.')
+    conn.query('BEGIN;');
+
+    try {
+      const lastGiveaway = await interaction.client.db.giveaways.getLastWon(interaction.user.id);
+      if (lastGiveaway == null) return interaction.reply("You cannot use this command at the moment.");
+      if (lastGiveaway.rep_given === true) {
+        return interaction.reply('A reputation point has already been given to this donor.')
+      }
+
+      const lastKey = await interaction.client.db.keys.get(lastGiveaway.key)
+      // This should never happen. We do it for the typing.
+      if (lastKey == null) return interaction.reply("No key was found on your latest giveaway.");
+
+      await interaction.client.db.users.incrementRep(lastKey.donor_id, increment, conn);
+      await interaction.client.db.giveaways.setRepGiven(lastGiveaway.id, true, conn);
+      await conn.query('COMMIT;');
+    } catch {
+      await conn.query('ROLLBACK;');
+    } finally {
+      conn.release();
     }
-
-    const lastKey = await interaction.client.db.keys.get(lastGiveaway.key)
-    // This should never happen. We do it for the typing.
-    if (lastKey == null) return interaction.reply("No key was found on your latest giveaway.");
-
-    await interaction.client.db.users.incrementRep(lastKey.donor_id, increment, conn);
-    await interaction.client.db.giveaways.setRepGiven(lastGiveaway.id, true, conn);
-    conn.release();
   }
 
   async good(interaction: CommandInteraction): Promise<void> {
