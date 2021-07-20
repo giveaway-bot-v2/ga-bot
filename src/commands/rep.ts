e621a7b1fd1aaf2cc0f9164c1d54be39750e94a9
Danny Harpigny <dannyhpy@disroot.org>
2021-07-20 13:45:48 +0200

    rep: use a middleware func to avoid duplicate code
    - change <RepCommand>.description
    
    - remove <RepCommand>[option].required properties
    
    - add method <RepCommand>.give() to remove duplicated code
    
    - <UserTable>.incrementRep() is no longer an upsert
    
    - add JSDoc to <GiveawayTable>.setRepGiven

--- src/commands/rep.ts
+++ src/commands/rep.ts
@@ -4,7 +4,7 @@ import type { PermissionString, CommandInteraction } from 'discord.js';
 
 export default class RepCommand extends Command {
   name = 'rep';
-  description = 'Either give a good or a bad reputation point to an user.';
+  description = 'Give the donator of your last won giveaway a reputation point.';
   permissions: Array<PermissionString> = ['SEND_MESSAGES'];
   DMOnly = true;
   options = [
@@ -12,17 +12,15 @@ export default class RepCommand extends Command {
       type: 'SUB_COMMAND' as const,
       name: 'good',
       description: 'Give a good reputation point',
-      required: true
     },
     {
       type: 'SUB_COMMAND' as const,
       name: 'bad',
       description: 'Give a bad reputation point',
-      required: true
     }
   ];
 
-  async good(interaction: CommandInteraction): Promise<void> {
+  async give(interaction: CommandInteraction, increment: number): Promise<void> {
     const conn = await interaction.client.db.connect();
     
     const lastGiveaway = await interaction.client.db.giveaways.getLastWon(interaction.user.id);
@@ -32,30 +30,22 @@ export default class RepCommand extends Command {
     }
 
     const lastKey = await interaction.client.db.keys.get(lastGiveaway.key)
+    // This should never happen. We do it for the typing.
     if (lastKey == null) return interaction.reply("No key was found on your latest giveaway.");
 
-    await interaction.client.db.users.incrementRep(lastKey.donor_id, 1, conn);
+    await interaction.client.db.users.incrementRep(lastKey.donor_id, increment, conn);
     await interaction.client.db.giveaways.setRepGiven(lastGiveaway.id, true, conn);
     conn.release();
+  }
+
+  async good(interaction: CommandInteraction): Promise<void> {
+    await this.give(interaction, 1)
     
     return interaction.reply('A good reputation point has been given.');
   }
 
   async bad(interaction: CommandInteraction): Promise<void> {
-    const conn = await interaction.client.db.connect();
-    
-    const lastGiveaway = await interaction.client.db.giveaways.getLastWon(interaction.user.id);
-    if (lastGiveaway == null) return interaction.reply("You cannot use this command at the moment.");
-    if (lastGiveaway.rep_given === true) {
-      return interaction.reply('A reputation point has already been given to this donor.')
-    }
-
-    const lastKey = await interaction.client.db.keys.get(lastGiveaway.key)
-    if (lastKey == null) return interaction.reply("No key was found on your latest giveaway.");
-
-    await interaction.client.db.users.incrementRep(lastKey.donor_id, -1, conn);
-    await interaction.client.db.giveaways.setRepGiven(lastGiveaway.id, true, conn);
-    conn.release();
+    await this.give(interaction, -1)
     
     return interaction.reply('A bad reputation point has been given.');
   }
